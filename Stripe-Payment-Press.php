f0d551ea0d6aa33bbaadf930c13ea44993ebdb4b
Marat Nepomnyashy <maratbn@gmail.com>
2015-07-26 15:08:47 -0700

    Added boilerplate for 'Stripe-Payment-Press.php'.

---
+++ Stripe-Payment-Press.php
@@ -0,0 +1,44 @@
+<?php
+/*
+  Plugin Name: Stripe-Payment-Press
+  Description: WordPress plugin for embedding Stripe checkouts via shortcodes.
+  Author: Marat Nepomnyashy
+  Author URI: http://www.maratbn.com
+  License: GPL3
+  Version: 0.0.1-development_unreleased
+  Text Domain: domain-plugin-Stripe-Payment-Press
+*/
+
+/*
+  Stripe-Payment-Press -- WordPress plugin for embedding Stripe checkouts via
+                          shortcodes.
+
+  Copyright (C) 2015  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
+
+  Version:        0.0.1-development_unreleased
+
+  Module:         Stripe-Payment-Press.php
+
+  Description:    Main PHP file for the WordPress plugin 'Stripe-Payment-Press'.
+
+  This file is part of Stripe-Payment-Press.
+
+  Licensed under the GNU General Public License Version 3.
+
+  Stripe-Payment-Press is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Stripe-Payment-Press is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Stripe-Payment-Press.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace plugin_Stripe_Payment_Press;
+
+?>
