8751d31d7c5436ac2ed1d10b7b69c1269febf48d
Marat Nepomnyashy <maratbn@gmail.com>
2015-11-14 20:17:31 -0800

    Activated the IS_MODE_RELEASE flag.

--- rainbowpaypress/RainbowPayPress.php
+++ rainbowpaypress/RainbowPayPress.php
@@ -44,7 +44,7 @@ namespace plugin_StripePaymentPress;
 const PLUGIN_VERSION = '1.0.0-development_unreleased_candidate';
 
 
-const IS_MODE_RELEASE = false;
+const IS_MODE_RELEASE = true;
 
 
 const PHP_VERSION_MIN_SUPPORTED = '5.4';
