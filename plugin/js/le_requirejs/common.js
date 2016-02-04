7ed3a14d47182808f3cb912a3a9224ab01541d00
Marat Nepomnyashy <maratbn@gmail.com>
2016-02-03 22:17:56 -0800

    Added new module 'common' to act as an aggregator for logic common between the admin and public UIs.

---
+++ plugin/js/le_requirejs/common.js
@@ -0,0 +1,43 @@
+/*
+  StripePaymentPress -- WordPress plugin for embedding Stripe checkouts via
+                        shortcodes.
+
+  Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
+
+  Version:        0.5.0-development_unreleased
+
+  Module:         js/le_requirejs/common.js
+
+  Description:    Aggregator module for logic common between the admin and public UIs.
+
+  This file is part of StripePaymentPress.
+
+  Licensed under the GNU General Public License Version 3.
+
+  StripePaymentPress is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  StripePaymentPress is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with StripePaymentPress.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+(function(define) {
+
+
+define(['backbone',
+        'jquery',
+        'underscore',
+        'util'
+    ], function() {
+    });
+
+
+})(_plugin_StripePaymentPress__define);
