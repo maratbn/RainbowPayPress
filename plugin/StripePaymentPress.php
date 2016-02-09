db6401b3b8d9c646ac06232e0a8532e8ae474ec4
Marat Nepomnyashy <maratbn@gmail.com>
2016-02-08 20:50:30 -0800

    Doing the action 'wp_ajax_stripe_payment_press__admin__charge' only when in the admin mode.

--- plugin/StripePaymentPress.php
+++ plugin/StripePaymentPress.php
@@ -67,8 +67,6 @@ require_once('StripePaymentPress_util.php');
 \add_action('admin_menu', '\\plugin_StripePaymentPress\\action_admin_menu');
 \add_action('admin_print_footer_scripts',
             '\\plugin_StripePaymentPress\\action_admin_print_footer_scripts');
-\add_action('wp_ajax_stripe_payment_press__admin__charge',
-            '\\plugin_StripePaymentPress\\action_wp_ajax_stripe_payment_press__admin__charge');
 \add_action('wp_ajax_stripe_payment_press__delete',
             '\\plugin_StripePaymentPress\\action_wp_ajax_stripe_payment_press__delete');
 \add_action('wp_ajax_stripe_payment_press__submit',
@@ -88,6 +86,9 @@ require_once('StripePaymentPress_util.php');
 
 
 if (\is_admin()) {
+    \add_action(
+        'wp_ajax_stripe_payment_press__admin__charge',
+        '\\plugin_StripePaymentPress\\action_wp_ajax_stripe_payment_press__admin__charge');
     \add_action(
         'wp_ajax_stripe_payment_press__admin__get_config',
         '\\plugin_StripePaymentPress\\action_wp_ajax_stripe_payment_press__admin__get_config');
