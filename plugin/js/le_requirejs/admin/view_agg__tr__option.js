eff5a3619671cf75a0c67cd842d30da46220d8e9
Marat Nepomnyashy <maratbn@gmail.com>
2016-02-14 20:39:42 -0800

    Incremented the VERSION to '0.8.0-development_unreleased'.

--- plugin/js/le_requirejs/admin/view_agg__tr__option.js
+++ plugin/js/le_requirejs/admin/view_agg__tr__option.js
@@ -4,7 +4,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        0.7.0-development_unreleased
+  Version:        0.8.0-development_unreleased
 
   Module:         js/le_requirejs/admin/view_agg__tr__option.js
 
