78441e2f265e8fca3f2a2a2ceed4f53c79ba79c4
Marat Nepomnyashy <maratbn@gmail.com>
2016-02-17 00:18:09 -0800

    Incremented the VERSION to '0.10.0-development_unreleased'.

--- plugin/js/le_requirejs/admin/view_agg__tr__option__string__email_notifications.js
+++ plugin/js/le_requirejs/admin/view_agg__tr__option__string__email_notifications.js
@@ -4,7 +4,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        0.9.0-development_unreleased
+  Version:        0.10.0-development_unreleased
 
   Module:         js/le_requirejs/admin/view_agg__tr__option__string__email_notifications.js
 
