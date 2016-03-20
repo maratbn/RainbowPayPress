464130bd8e8436d74a4ad39eff824f4279c9950d
Marat Nepomnyashy <maratbn@gmail.com>
2016-03-19 23:49:07 -0700

    Incremented the VERSION to '2.0.0'.

--- rainbowpaypress/js/le_requirejs/admin/view_agg__tr__w_header.js
+++ rainbowpaypress/js/le_requirejs/admin/view_agg__tr__w_header.js
@@ -4,7 +4,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        2.0.0-development_unreleased_candidate
+  Version:        2.0.0
 
   Module:         rainbowpaypress/js/le_requirejs/admin/view_agg__tr__w_header.js
 
