89439ea9388728d7b7184fc10c5a190881f7184a
Marat Nepomnyashy <maratbn@gmail.com>
2016-03-19 23:35:49 -0700

    Incremented the VERSION to '2.0.0-development_unreleased'.

--- rainbowpaypress/js/le_requirejs/admin/model_orig__transaction.js
+++ rainbowpaypress/js/le_requirejs/admin/model_orig__transaction.js
@@ -4,7 +4,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        1.1.0-development_unreleased
+  Version:        2.0.0-development_unreleased
 
   Module:         rainbowpaypress/js/le_requirejs/admin/model_orig__transaction.js
 
