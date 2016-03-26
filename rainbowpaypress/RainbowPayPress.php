8232c37c2797789ed55072369e6a83bb93ac65cf
Marat Nepomnyashy <maratbn@gmail.com>
2016-03-25 21:02:18 -0700

    Changed the plugin quick link to info / settings to be an 'About' link to the help page.

--- rainbowpaypress/RainbowPayPress.php
+++ rainbowpaypress/RainbowPayPress.php
@@ -615,8 +615,8 @@ function action_wp_print_footer_scripts() {
 
 function filter_plugin_action_links($arrLinks) {
     \array_push($arrLinks,
-                '<a href=\'' . getUrlInfoSettings() . '\'>'
-                          . \__('Info / Settings', DOMAIN_PLUGIN_RAINBOW_PAY_PRESS) . '</a>');
+                '<a href=\'' . \admin_url('admin.php?page=' . SLUG_HELP) . '\'>'
+                          . \__('About', DOMAIN_PLUGIN_RAINBOW_PAY_PRESS) . '</a>');
     \array_push($arrLinks,
                 '<a href=\'' . \plugin_dir_url(__FILE__) . 'LICENSE\'>'
                           . \__('License', DOMAIN_PLUGIN_RAINBOW_PAY_PRESS) . '</a>');
