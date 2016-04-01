12c23644ee345d6bbfcea10e185ea7188f46c793
Marat Nepomnyashy <maratbn@gmail.com>
2016-03-31 23:48:15 -0700

    Incremented the VERSION to '2.2.0-development_unreleased_candidate'.

--- rainbowpaypress/RainbowPayPress_util.php
+++ rainbowpaypress/RainbowPayPress_util.php
@@ -5,7 +5,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        2.2.0-development_unreleased
+  Version:        2.2.0-development_unreleased_candidate
 
   Module:         rainbowpaypress/RainbowPayPress_util.php
 
--- rainbowpaypress/js/le_requirejs/common.js
+++ rainbowpaypress/js/le_requirejs/common.js
@@ -4,7 +4,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        2.2.0-development_unreleased
+  Version:        2.2.0-development_unreleased_candidate
 
   Module:         rainbowpaypress/js/le_requirejs/common.js
 
