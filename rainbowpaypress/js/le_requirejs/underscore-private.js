c423b1e9fbc1c4abf5c93efb2bc37a210b9e28c0
Marat Nepomnyashy <maratbn@gmail.com>
2016-04-11 01:02:30 -0700

    Incremented the VERSION to '3.1.0-development_unreleased'.

--- rainbowpaypress/js/le_requirejs/underscore-private.js
+++ rainbowpaypress/js/le_requirejs/underscore-private.js
@@ -4,7 +4,7 @@
 
   Copyright (C) 2015-2016  Marat Nepomnyashy  http://maratbn.com  maratbn@gmail
 
-  Version:        3.0.0-development_unreleased
+  Version:        3.1.0-development_unreleased
 
   Module:         rainbowpaypress/js/le_requirejs/underscore-private.js
 
